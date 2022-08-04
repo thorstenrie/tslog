32c87d7aefcf56bfdefeda559e95f1c09d48f219
thorstenrie <author@lichtprotoss.com>
2022-08-05 00:22:47 +0200

    Adding Fuzz testing

--- log_test.go
+++ log_test.go
@@ -2,21 +2,37 @@ package tslog
 
 import (
 	"bufio"
+	"fmt"
 	"os"
-	"path/filepath"
 	"testing"
+	"time"
 )
 
 var (
-	infoText  string = "info test"
-	errorText string = "error test"
+	testcases = []testcase{
+		{errorPrefix, "test"},
+		{infoPrefix, " "},
+		{errorPrefix, "Hello World!"},
+		{infoPrefix, "!12345"},
+	}
 )
+
+type testcase struct {
+	prefix, in string
+}
+
+type testStruct struct {
+	in   string
+	want testcase
+}
+
+type testFunc func(*testing.T, *testStruct)
 
 func TestEmpty(t *testing.T) {
 	if err := setEnv(""); err != nil {
 		t.Errorf("set empty env TS_LOGFILE failed: %v", err)
 	}
-	testLog()
+	testLog(testcases)
 }
 
 func TestNotSet(t *testing.T) {
@@ -24,91 +40,183 @@ func TestNotSet(t *testing.T) {
 		t.Errorf("unset env TS_LOGFILE failed: %v", err)
 	}
 	Reset()
-	testLog()
+	testLog(testcases)
 }
 
 func TestDirectory1(t *testing.T) {
 	if err := setEnv("/tmp/"); err != nil {
 		t.Errorf("set env TS_LOGFILE = /tmp/ failed: %v", err)
 	}
-	testLog()
+	testLog(testcases)
 }
 
 func TestDirectory2(t *testing.T) {
 	if err := setEnv("/tmp"); err != nil {
 		t.Errorf("set env TS_LOGFILE = /tmp failed: %v", err)
 	}
-	testLog()
+	testLog(testcases)
 }
 
 func TestStdout(t *testing.T) {
 	if err := setEnv("stdout"); err != nil {
 		t.Errorf("set env TS_LOGFILE = stdout failed: %v", err)
 	}
-	testLog()
+	testLog(testcases)
 }
 
 func TestDiscard(t *testing.T) {
 	if err := setEnv("discard"); err != nil {
 		t.Errorf("set env TS_LOGFILE = discard failed: %v", err)
 	}
-	testLog()
+	testLog(testcases)
 }
 
 func TestInit(t *testing.T) {
-	testLog()
+	testLog(testcases)
 }
 
-func TestFile(t *testing.T) {
-	fn := filepath.Join(os.TempDir(), "tslog_test.txt")
-	if _, err := os.Create(fn); err != nil {
-		t.Errorf("creating %v failed: %v", fn, err)
+func BenchmarkInfoLog(b *testing.B) {
+	tmpLog(b).Close()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		testLog(testcases)
 	}
-	if err := setEnv(fn); err != nil {
-		t.Errorf("set env TS_LOGFILE = %v failed: %v", fn, err)
-	}
-	testLog()
-	f, err := os.Open(fn)
+}
+
+type testingType interface {
+	*testing.T | *testing.B | *testing.F
+	Errorf(format string, a ...any)
+}
+
+func tmpLog[T testingType](tt T) *os.File {
+	f, err := os.CreateTemp(os.TempDir(), "tslog_test_*")
 	if err != nil {
-		t.Errorf("open logfile %v failed: %v", fn, err)
+		f.Close()
+		tt.Errorf("creating %v failed: %v", f.Name(), err)
+		return os.Stdout
 	}
+	if err := setEnv(f.Name()); err != nil {
+		tt.Errorf("set env TS_LOGFILE = %v failed: %v", f.Name(), err)
+	}
+	return f
+}
+
+func FuzzInfo(f *testing.F) {
+	for _, tc := range testcases {
+		f.Add(tc.in)
+	}
+	f.Fuzz(func(t *testing.T, a string) {
+		tc := []testcase{{prefix: infoPrefix, in: a}}
+		testWrapper(t, tc, testLength)
+		testWrapper(t, tc, testPrefix)
+		testWrapper(t, tc, testMessage)
+	})
+}
+
+func testWrapper(t *testing.T, tc []testcase, tf testFunc) {
+	f := tmpLog(t)
+	fmt.Println(f.Name())
+	testLog(tc)
 	scanner := bufio.NewScanner(f)
-	i := 0
-	for ; scanner.Scan(); i++ {
+	var want testcase
+	for i := 0; scanner.Scan(); i++ {
 		if ln := scanner.Text(); len(ln) > 0 {
-			if len(ln) < mLen() {
-				f.Close()
-				t.Errorf("Line %v in logfile %v too short", ln, fn)
-			} else if (ln[0:len(infoPrefix)] != infoPrefix) && (ln[0:len(errorPrefix)] != errorPrefix) {
-				f.Close()
-				t.Errorf("Prefix not found; %v or %v expected", infoPrefix, errorPrefix)
-			} else if (ln[len(ln)-len(infoText):] != infoText) && (ln[len(ln)-len(errorText):] != errorText) {
-				f.Close()
-				t.Errorf("Text not found; %v or %v expected", infoText, errorText)
+			if i < len(tc) {
+				want = tc[i]
+			} else {
+				want = testcase{prefix: errorPrefix, in: ""}
+				t.Errorf("log file with %d lines expected, but got additional log message %v", len(testcases), ln)
 			}
+			tf(t, &testStruct{in: ln, want: want})
 		}
 	}
-	f.Close()
+	if err := f.Close(); err != nil {
+		t.Errorf("closing %v failed: %v", f.Name(), err)
+	}
 }
 
-func testLog() {
-	I.Print(infoText)
-	E.Print(errorText)
+func testLength(t *testing.T, tc *testStruct) {
+	if t == nil {
+		E.Fatalln("nil pointer")
+	}
+	if tc == nil {
+		t.Errorf("nil pointer")
+		return
+	}
+	minl := len(tc.want.prefix) + len(tc.want.in) + len(time.Now().Format("2009/01/23 01:23:23")) + 2 /*spaces*/ + 2 /*colons*/
+	actl := len(tc.in)
+	if actl < minl {
+		t.Errorf("minimum length %d expected, but length is %d", minl, actl)
+	}
+}
+
+func testPrefix(t *testing.T, tc *testStruct) {
+	if t == nil {
+		E.Fatalln("nil pointer")
+	}
+	if tc == nil {
+		t.Errorf("nil pointer")
+		return
+	}
+	minl := len(tc.want.prefix)
+	actl := len(tc.in)
+	if actl < minl {
+		t.Errorf("log message length %d shorter than length %d of prefix %v", actl, minl, tc.want.prefix)
+		return
+	}
+	actp := tc.in[0:minl]
+	if actp != tc.want.prefix {
+		t.Errorf("expected prefix %v but got %v", tc.want.prefix, actp)
+	}
+}
+
+func testMessage(t *testing.T, tc *testStruct) {
+	if t == nil {
+		E.Fatalln("nil pointer")
+	}
+	if tc == nil {
+		t.Errorf("nil pointer")
+		return
+	}
+	minl := len(tc.want.in)
+	actl := len(tc.in)
+	if actl < minl {
+		t.Errorf("log message length %d shorter than length %d of message %v", actl, minl, tc.want.in)
+		return
+	}
+	actm := tc.in[len(tc.in)-minl:]
+	if actm != tc.want.in {
+		t.Errorf("expected log message %v but got %v", tc.want.in, actm)
+	}
+}
+
+func TestLogLength(t *testing.T) {
+	testWrapper(t, testcases, testLength)
+}
+
+func TestLogPrefix(t *testing.T) {
+	testWrapper(t, testcases, testPrefix)
+}
+
+func TestLogMessage(t *testing.T) {
+	testWrapper(t, testcases, testMessage)
+}
+
+func testLog(tc []testcase) {
+	for i := range tc {
+		tc := tc[i]
+		if tc.prefix == infoPrefix {
+			I.Print(tc.in)
+		} else if tc.prefix == errorPrefix {
+			E.Print(tc.in)
+		} else {
+			E.Printf("expected prefix %v or %v, but got prefix %v for log message %v", infoPrefix, errorPrefix, tc.prefix, tc.in)
+		}
+	}
 }
 
 func setEnv(fn string) error {
 	err := os.Setenv("TS_LOGFILE", fn)
 	Reset()
 	return err
-}
-
-func mLen() int {
-	v := []int{len(infoPrefix), len(errorPrefix), len(infoText), len(errorText)}
-	min := v[0]
-	for i := range v {
-		if i < min {
-			min = i
-		}
-	}
-	return min
 }
