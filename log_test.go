6f6971a6e36a4bff1d39056f3a4e3770ccacdfc2
thorstenrie <author@lichtprotoss.com>
2022-08-11 23:11:45 +0200

    Add comments

--- log_test.go
+++ log_test.go
@@ -146,41 +146,66 @@ func BenchmarkLog(b *testing.B) {
 	}
 }
 
+// FuzzInfo conducts fuzzing on log messages and checks for
+// errors. The checks include the length of the log message,
+// the prefix and the correct logging of the fuzzed message.
+func FuzzInfo(f *testing.F) {
+	// Addition of testcases to the seed corpus
+	for _, tc := range testcases {
+		f.Add(tc.in)
+	}
+	// Fuzz target
+	f.Fuzz(func(t *testing.T, a string) {
+		// Create testcase as informational log with fuzzing applied
+		// to the log message
+		tc := testcase{prefix: infoPrefix, in: a}
+		// Test log message length
+		testWrapper(t, tc, testLength)
+		// Test prefix
+		testWrapper(t, tc, testPrefix)
+		// Test log message text
+		testWrapper(t, tc, testMessage)
+	})
+}
+
+// tmpLog creates a temp log file tslog_test_* in the temp directory.
+// The env variable TS_LOGFILE is set accordingly.
+// tmpLog returns the temp file. In case of errors tmpLog returns Stdout.
 func tmpLog[T testingtype](tt T) *os.File {
+	// Create temp log file tslog_test_* in the temp directory
 	f, err := os.CreateTemp(os.TempDir(), "tslog_test_*")
+	// In case of an error fall back to Stdout for logging
 	if err != nil {
 		f.Close()
 		tt.Errorf("creating %v failed: %v", f.Name(), err)
 		return os.Stdout
 	}
+	// Set TS_LOGFILE to temp log file tslog_test_*
 	if err := setEnv(f.Name()); err != nil {
 		tt.Errorf("set env TS_LOGFILE = %v failed: %v", f.Name(), err)
 	}
+	// Return temp log file tslog_test_*
 	return f
 }
 
-func FuzzInfo(f *testing.F) {
-	for _, tc := range testcases {
-		f.Add(tc.in)
-	}
-	f.Fuzz(func(t *testing.T, a string) {
-		tc := testcase{prefix: infoPrefix, in: a}
-		testWrapper(t, tc, testLength)
-		testWrapper(t, tc, testPrefix)
-		testWrapper(t, tc, testMessage)
-	})
-}
-
+// testWrapper logs a testcase into a temp file and checks the
+// result with tf.
 func testWrapper(t *testing.T, tc testcase, tf testfunc) {
+	// Create temp file and set env variable
 	f := tmpLog(t)
+	// Log testcase
 	testLog(tc)
+
 	var want testcase
+	// Read log file
 	in, err := io.ReadAll(f)
 	if err != nil {
 		t.Errorf("open %v failed: %v", f.Name(), err)
 		return
 	}
+	// Check log file with tf
 	tf(t, &testcheck{in: string(in), want: want})
+	// Close temp log file
 	if err := f.Close(); err != nil {
 		t.Errorf("closing %v failed: %v", f.Name(), err)
 	}
