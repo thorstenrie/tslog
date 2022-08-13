bce809076cf4d9b7ea25ad5a9c4bcf02a6472b3d
thorstenrie <author@lichtprotoss.com>
2022-08-13 13:07:04 +0200

    Add copyright note

--- log_test.go
+++ log_test.go
@@ -1,3 +1,6 @@
+// Copyright (c) 2022 thorstenrie
+// All Rights Reserved. Use is governed with GNU Affero General Public License v3.0
+// that can be found in the LICENSE file.
 package tslog
 
 // Import standard library packages.
